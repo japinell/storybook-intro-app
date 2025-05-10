d7f1c28cab27118447ff07c6238d60a94faedc07
Jose A. Pinell <japinell@yahoo.com>
2025-05-10 16:33:38 -0700

    Added property descriptions

--- src/components/ButtonCounter.stories.jsx
+++ src/components/ButtonCounter.stories.jsx
@@ -2,28 +2,66 @@
 import { ButtonCounter } from "./ButtonCounter";
 
 export default {
-    title: 'Counter/ButtonCounter',
-    component: ButtonCounter,
-    tags: ['autodocs'],
-    argTypes: {
-        backgroundColor: {
-           control: 'color' 
-        }
+  title: "Counter/ButtonCounter",
+  component: ButtonCounter,
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: {
+      control: "color",
+      description: "Background color of the button",
+      table: {
+        defaultValue: {
+          summary: "#bcebe6",
+        },
+      }
+    },
+    label: {
+        control: "text",
+        description: "Label of the button",
+        table: {
+          defaultValue: {
+            summary: "Click me",
+          },
+        },
+    },
+    size: {
+        control: "text",
+        description: "Size of the button",
+        table: {
+          defaultValue: {
+            summary: "large",
+          },
+        },
     }
-}
+  },
+};
 
 // Large button story
 export const Large = {
-    args: {
-        size: 'large',
-        label: 'Large Button',
+  args: {
+    size: "large",
+    label: "Large Button",
+  },
+  parameters: {
+    docs: {
+        description: {
+            story: "This is a large button with a default label.",
+        },
     }
-}
+  }
+};
 
 // Small button story
 export const Small = {
-    args: {
-        size: 'small',
-        label: 'Small Button',
+  args: {
+    size: "small",
+    label: "Small Button",
+  },
+  parameters: {
+    docs: {
+        description: {
+            story: "This is a small button with a default label.",
+        },
     }
-}
+  }
+};
